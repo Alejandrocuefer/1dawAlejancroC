c8a96937fcbba222a6159e12ad58365cbec997e8
AlexC <alexcuevasfer@gmail.com>
2020-04-22 22:58:16 +0200

    Añade metodo main al bingo + la prueba

--- 1dawAlejancroC/src/main/java/com/mycompany/dawalejancroc/JuegoBingo.java
+++ 1dawAlejancroC/src/main/java/com/mycompany/dawalejancroc/JuegoBingo.java
@@ -5,10 +5,59 @@
  */
 package com.mycompany.dawalejancroc;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alex
  */
 public class JuegoBingo {
-    
+    public static void main(String[] args) {
+        // Creamos 2 objetos, un carton y un bombo
+        Carton carton = new Carton();
+        Bombo bombo = new Bombo();
+
+        //Imprimo el carton, primero lo relleno y lo muestro
+        System.out.println("Imprimiento carton...");
+        carton.rellenarCarton();
+        carton.mostarCarton();
+
+//        Imprimir el número de la bola sacada.
+        System.out.println("Sacamos una bola...");
+        System.out.println(bombo.sacarBola());
+        System.out.println("------------------");
+
+//        Comprobar si esa bola está presente en el cartón y proceder a “tacharla”, 
+//        informando con un mensaje por consola.
+        carton.tacharCasilla(bombo.sacarBola());
+
+//        Comprobar si, después de haber tachado alguna casilla, se hace línea 
+//        (se tachan todos los números de una fila del cartón). Entonces la aplicación 
+//        cantará "línea", mostrando un mensaje, indicando la fila en la que se ha 
+//        producido.
+        carton.comprobarLinea();
+
+//        Comprobar si, después de haber cantado línea, se puede cantar bingo 
+//        (se tachan todos los números del cartón). En este caso la aplicación 
+//        cantará "bingo", mostrando un mensaje también y el juego termina.
+        carton.comprobarBingo();
+
+//        También se debe mostrar el estado del cartón (números tachados y números activos).
+        carton.mostarCarton();
+
+//        Para sacar una bola nueva el usuario debe pulsar alguna tecla.
+        String seguir;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Pulse una tecla para continuar");
+
+        try {
+
+            seguir = teclado.nextLine();
+            System.out.println(bombo.sacarBola());
+
+        } catch (Exception e) {
+        }
+    }
 }
